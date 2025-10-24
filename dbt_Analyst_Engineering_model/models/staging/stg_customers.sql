ffc29578b6c3d889b3e2e203d18eed602b1b980f
Pooneh2023 <126458612+Pooneh2023@users.noreply.github.com>
2025-10-24 13:15:58 +0200

    Update materialization to table for stg_customers
    Changed materialization from view to table for stg_customers.

--- dbt_Analyst_Engineering_model/models/staging/stg_customers.sql
+++ dbt_Analyst_Engineering_model/models/staging/stg_customers.sql
@@ -1,9 +1,8 @@
 -- models/staging/stg_customers.sql
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 select distinct on (customer_id)
   customer_id,
   country
 from {{ ref('stg_invoices') }}
 where customer_id is not null
-order by customer_id, invoice_ts desc, (country is null)
