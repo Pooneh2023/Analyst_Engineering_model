00aafcb3a68083479621c97ff82d378c3918756b
Pooneh2023 <126458612+Pooneh2023@users.noreply.github.com>
2025-10-24 16:56:14 +0200

    Add price_updated_at field to prep_products model

--- dbt_Analyst_Engineering_model/models/intermediate/prep_products.sql
+++ dbt_Analyst_Engineering_model/models/intermediate/prep_products.sql
@@ -1,9 +1,10 @@
 {{ config(materialized='table') }}
 
 select
-  stock_code,
-  coalesce(nullif(description, ''), 'unknown') as description,
-  unit_price
+    stock_code,
+    coalesce(nullif(description, ''), 'unknown') as description,
+    unit_price,
+    price_updated_at  
 from {{ ref('stg_products') }}
 where unit_price is not null
-  and unit_price >= 0 -- remove invalid product prices
+  and unit_price >= 0  
